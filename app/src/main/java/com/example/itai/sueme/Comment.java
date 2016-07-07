58c7302a2751d52347f8a63a1e43bfd687c2f8b4
Nadav Luzzato <nadav2051@gmail.com>
2016-07-07 23:04:54 +0300

    Added forgotten field in the Comment (comment id)

--- app/src/main/java/com/example/itai/sueme/Comment.java
+++ app/src/main/java/com/example/itai/sueme/Comment.java
@@ -5,13 +5,16 @@ package com.example.itai.sueme;
  */
 public class Comment {
 
+    private int CommentID;                      // ID of Comment from DB.
     private int CommentatorID;                  // ID of commentator.
     private String CommentatorDisplayName;      // Display name of commentator.
     private String CommentContent;              // Content of the comment.
     private int ArticleID;                      // Article associated with this comment.
 
     /* Constructors */
-    public Comment(int commentatorID, String commentatorDisplayName, String commentContent, int articleID) {
+
+    public Comment(int commentID, int commentatorID, String commentatorDisplayName, String commentContent, int articleID) {
+        CommentID = commentID;
         CommentatorID = commentatorID;
         CommentatorDisplayName = commentatorDisplayName;
         CommentContent = commentContent;
@@ -25,6 +28,10 @@ public class Comment {
 
     public String getCommentatorDisplayName() {
         return CommentatorDisplayName;
+    }
+
+    public int getCommentID() {
+        return CommentID;
     }
 
     public String getCommentContent() {
@@ -42,6 +49,10 @@ public class Comment {
 
     public void setCommentatorDisplayName(String commentatorDisplayName) {
         CommentatorDisplayName = commentatorDisplayName;
+    }
+
+    public void setCommentID(int commentID) {
+        CommentID = commentID;
     }
 
     public void setCommentContent(String commentContent) {
