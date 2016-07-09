702c93003e385c671f8bf27d5b5eee1b1d7358b3
Nadav Luzzato <nadav2051@gmail.com>
2016-07-09 19:04:03 +0300

    Added article listview adapter

---
+++ app/src/main/java/com/example/itai/sueme/ArticleListViewAdapter.java
@@ -0,0 +1,39 @@
+package com.example.itai.sueme;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by nadav on 7/9/2016.
+ */
+public class ArticleListViewAdapter extends ArrayAdapter<Article> {
+    public ArticleListViewAdapter(Context context, ArrayList<Article> articles) {
+        super(context, 0, articles);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        Article article = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.listview_home_article, parent, false);
+        }
+        // Lookup view for data population
+        TextView author = (TextView) convertView.findViewById(R.id.AuthorArticleViewText);
+        TextView content = (TextView) convertView.findViewById(R.id.ContentArticleViewText);
+        TextView title = (TextView) convertView.findViewById(R.id.ArticleNameArticleViewText);
+        // Populate the data into the template view using the data object
+        title.setText(article.getArticleTitle());
+        author.setText(article.getCreatorDisplayName());
+        content.setText(article.getArticleContent());
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/itai/sueme/HomeActivity.java
+++ app/src/main/java/com/example/itai/sueme/HomeActivity.java
@@ -8,21 +8,38 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by nadav on 7/9/2016.
  */
 public class HomeActivity extends AppCompatActivity {
-
+    ArrayList<Article> articles = new ArrayList<Article>();
+    ArticleListViewAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        articles.add(new Article(0, "Title", "long description goes here", null, 0, "nadav", null));
+        articles.add(new Article(1, "secpmdToit", "long description goes here", null, 0, "nadav", null));
+        articles.add(new Article(2, "asfopjmasp", "long description goes here", null, 0, "nadav", null));
+        articles.add(new Article(3, "asofdk", "wqofjpqowejgf3pqotjg3p21qojt32pogjpwojpojgwepogjwepo", null, 0, "nadav", null));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         // Change welcome text
         String name = LoginActivity.ActiveUser.getName();
         ((TextView) findViewById(R.id.WelcomeHomeText)).setText("Welcome, " + name + "!");
+        PopulateListView();
+    }
 
+    private void PopulateListView() {
+        ListView lv = (ListView) findViewById(R.id.ArticleHomeListView);
+        arrayAdapter = new ArticleListViewAdapter(
+                this,
+                articles );
+        lv.setAdapter(arrayAdapter);
     }
 
 }
