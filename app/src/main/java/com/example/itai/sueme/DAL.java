1e67301554b7b3b57f0766abb68fd6af0e20b53b
itai-m <Itaimichael@gmail.com>
2016-07-09 09:03:35 +0300

    add the appController

--- app/src/main/java/com/example/itai/sueme/DAL.java
+++ app/src/main/java/com/example/itai/sueme/DAL.java
@@ -1,40 +1,47 @@
 package com.example.itai.sueme;
 
-import android.app.VoiceInteractor;
-import java.io.UnsupportedEncodingException;
-import java.util.Map;
-import org.json.JSONException;
-import org.json.JSONObject;
-import com.android.volley.NetworkResponse;
-import com.android.volley.ParseError;
+
+import android.util.Log;
+
 import com.android.volley.Request;
+
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.Response.ErrorListener;
-import com.android.volley.Response.Listener;
-import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.lang.reflect.Method;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class DAL {
 
 
     public int addUser(User user){
-        String uri = String.format(Constant.DataBase.URL);
-
-        StringRequest myReq = new StringRequest(Method.GET,
-                uri,
-                createMyReqSuccessListener(),
-                createMyReqErrorListener());
-        queue.add(myReq);
-
+        String url = String.format(Constant.DataBase.URL);
+        url = "http://sueme.berry-games.com/api.php?todo=insertUser&Name=[name]&Email=[Email]&Phonenumber=[phone]&Location=[Location]&Lawyer=0";
+        JSONObject json = getJson(url);
         return 0;
+    }
+    private JSONObject getJson(String url){
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("", "Response: " + response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("", "Error");
+
+            }
+        });
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+        return jsObjRequest.;
     }
 }
